204668ee2581e56012d0fa43b35b5f6b78b66f3d
Dominik Chat <dominik.sz.chat@gmail.com>
2023-05-04 12:39:40 +0200

    peripheral: Add gpio and afio headers
    Signed-off-by: Dominik Chat <dominik.sz.chat@gmail.com>

---
+++ include/peripheral/afio.h
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2023 Dominik Chat
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef _PERIPHERAL_AFIO_H_
+#define _PERIPHERAL_AFIO_H_
+
+#include <stdint.h>
+
+#define AFIO_REG_OFFSET			(0x40010000ul)
+
+/* PCFR1 */
+#define AFIO_PCFR1_SWCFG_MASK		(0x07ul << 24)
+#define AFIO_PCFR1_SWCFG_SWD		(0x00ul << 24)
+#define AFIO_PCFR1_SWCFG_GPIO		(0x04ul << 24)
+
+#define AFIO_PCFR1_TIM1_IREMAP_MASK	(0x01ul << 23)
+#define AFIO_PCFR1_TIM1_IREMAP_EXT	(0x00ul << 23)
+#define AFIO_PCFR1_TIM1_IREMAP_LSI	(0x01ul << 23)
+
+#define AFIO_PCFR1_I2C1_REMAP_MASK	((0x01ul << 22) | (0x01ul << 1))
+#define AFIO_PCFR1_I2C1_REMAP_DEF	((0x00ul << 22) | (0x00ul << 1))
+#define AFIO_PCFR1_I2C1_REMAP_PD10	((0x00ul << 22) | (0x01ul << 1))
+#define AFIO_PCFR1_I2C1_REMAP_PC56	((0x01ul << 22) | (0x00ul << 1))
+
+#define AFIO_PCFR1_USART1_REMAP_MASK	((0x01ul << 21) | (0x01ul << 2))
+#define AFIO_PCFR1_USART1_REMAP_DEF	((0x00ul << 21) | (0x00ul << 2))
+#define AFIO_PCFR1_USART1_REMAP_PD01	((0x00ul << 21) | (0x01ul << 2))
+#define AFIO_PCFR1_USART1_REMAP_PD65	((0x01ul << 21) | (0x00ul << 2))
+#define AFIO_PCFR1_USART1_REMAP_PC01	((0x01ul << 21) | (0x01ul << 2))
+
+#define AFIO_PCFR1_ADC_ETRGREG_RM_MASK	(0x01ul << 18)
+#define AFIO_PCFR1_ADC_ETRGREG_RM_PD3	(0x00ul << 18)
+#define AFIO_PCFR1_ADC_ETRGREG_RM_PC2	(0x01ul << 18)
+/* Something is wrong here in docs */
+#define AFIO_PCFR1_ADC_ETRGINJ_RM_MASK	(0x01ul << 17)
+#define AFIO_PCFR1_ADC_ETRGINJ_RM_PD3	(0x00ul << 17)
+#define AFIO_PCFR1_ADC_ETRGINJ_RM_PC2	(0x01ul << 17)
+
+#define AFIO_PCFR1_ADC_PA12_RM_MASK	(0x01ul << 15)
+#define AFIO_PCFR1_ADC_PA12_RM_GPIO	(0x00ul << 15)
+#define AFIO_PCFR1_ADC_PA12_RM_XTAL	(0x01ul << 15)
+
+#define AFIO_PCFR1_TIM2RM_MASK		(0x03ul << 8)
+#define AFIO_PCFR1_TIM2RM_DEF		(0x00ul << 8)
+#define AFIO_PCFR1_TIM2RM_C5C2D2C1	(0x01ul << 8)
+#define AFIO_PCFR1_TIM2RM_C1D3C0D7	(0x02ul << 8)
+#define AFIO_PCFR1_TIM2RM_C1C7D6D5	(0x03ul << 8)
+
+#define AFIO_PCFR1_TIM1RM_MASK		(0x03ul << 6)
+#define AFIO_PCFR1_TIM1RM_DEF		(0x00ul << 6)
+#define AFIO_PCFR1_TIM1RM_C6C7C0D3	(0x01ul << 6)
+#define AFIO_PCFR1_TIM1RM_D2A1C3C4	(0x02ul << 6)
+#define AFIO_PCFR1_TIM1RM_C4C7C5D4	(0x03ul << 6)
+
+#define AFIO_PCFR1_SPI1_REMAP_MASK	(0x01ul << 0)
+#define AFIO_PCFR1_SPI1_REMAP_DEF	(0x00ul << 0)
+#define AFIO_PCFR1_SPI1_REMAP_REM	(0x01ul << 0)
+
+/* EXTICR */
+#define AFIO_EXTICR_EXTI_MASK(x)	(0x03ul << ((x) * 2))
+#define AFIO_EXTICR_EXTI_PA(x)		(0x00ul << ((x) * 2))
+#define AFIO_EXTICR_EXTI_PC(x)		(0x02ul << ((x) * 2))
+#define AFIO_EXTICR_EXTI_PD(x)		(0x03ul << ((x) * 2))
+
+typedef struct {
+	uint32_t RESERVED;		/**< RESERVED */
+	uint32_t PCFR1;			/**< Port input data */
+	uint32_t EXTICR;		/**< Port output data */
+} per_afio_t;
+
+volatile per_afio_t *AFIO = (volatile per_afio_t *)AFIO_REG_OFFSET;
+
+#endif /* _PERIPHERAL_AFIO_H_ */
---
+++ include/peripheral/gpio.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2023 Dominik Chat
+ *
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+#ifndef _PERIPHERAL_GPIO_H_
+#define _PERIPHERAL_GPIO_H_
+
+#include <stdint.h>
+
+#define GPIO_A_REG_OFFSET	(0x40010800ul)
+#define GPIO_C_REG_OFFSET	(0x40011000ul)
+#define GPIO_D_REG_OFFSET	(0x40011400ul)
+
+/* CFGLR */
+#define GPIO_CFGLR_CNF_MASK(x)	(0x03ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_ANA(x)	(0x00ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_FLT(x)	(0x01ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_PULL(x)	(0x02ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_PUSH(x)	(0x00ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_OPEN(x)	(0x01ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_MPUSH(x)	(0x02ul << (((x) * 4) + 2))
+#define GPIO_CFGLR_CNF_MOPEN(x)	(0x03ul << (((x) * 4) + 2))
+
+#define GPIO_CFGLR_MODE_MASK(x)	(0x03ul << ((x) * 4))
+#define GPIO_CFGLR_MODE_INP(x)	(0x00ul << ((x) * 4))
+#define GPIO_CFGLR_MODE_10M(x)	(0x01ul << ((x) * 4))
+#define GPIO_CFGLR_MODE_2M(x)	(0x02ul << ((x) * 4))
+#define GPIO_CFGLR_MODE_50M(x)	(0x03ul << ((x) * 4))
+
+/* INDR */
+#define GPIO_INDR_IDR_MASK	(0xFFul << 0)
+#define GPIO_INDR_IDR(x)	(0x01ul << (x))
+
+/* OUTDR */
+#define GPIO_OUTDR_ODR_MASK	(0xFFul << 0)
+#define GPIO_OUTDR_ODR(x)	(0x01ul << (x))
+
+/* BSHR */
+#define GPIO_BSHR_BR_MASK	(0xFFul << 16)
+#define GPIO_BSHR_BR(x)		(0x01ul << ((x) + 16))
+
+#define GPIO_BSHR_BS_MASK	(0xFFul << 0)
+#define GPIO_BSHR_BS(x)		(0x01ul << (x))
+
+/* BCR */
+#define GPIO_BCR_BR_MASK	(0xFFul << 0)
+#define GPIO_BCR_BR(x)		(0x01ul << (x))
+
+/* LCKR */
+#define GPIO_LCKR_LCKK_MASK	(0x01ul << 8)
+#define GPIO_LCKR_LCKK		(0x01ul << 8)
+
+#define GPIO_LCKR_LCK_MASK	(0xFFul << 0)
+#define GPIO_LCKR_LCK(x)	(0x01ul << (x))
+
+typedef struct {
+	uint32_t CFGLR;		/**< Port configuration low */
+	uint32_t RESERVED;	/**< RESERVED */
+	uint32_t INDR;		/**< Port input data */
+	uint32_t OUTDR;		/**< Port output data */
+	uint32_t BSHR;		/**< Port set/reset */
+	uint32_t BCR;		/**< Port reset */
+	uint32_t LCKR;		/**< Port configuration lock */
+} per_gpio_t;
+
+volatile per_gpio_t *GPIO_A = (volatile per_gpio_t *)GPIO_A_REG_OFFSET;
+volatile per_gpio_t *GPIO_C = (volatile per_gpio_t *)GPIO_C_REG_OFFSET;
+volatile per_gpio_t *GPIO_D = (volatile per_gpio_t *)GPIO_D_REG_OFFSET;
+
+#endif /* _PERIPHERAL_GPIO_H_ */
